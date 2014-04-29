50bbcb6d09a1b119dbc2e261da93c09484e4cd20
Julliano <julliano.rn@gmail.com>
2014-04-29 20:50:23 -0300

    Criada a tela do usuario e vinculada a tela principal

--- implementacao/src/br/ufg/inf/es/sinoa/TelaUsuario.java
+++ implementacao/src/br/ufg/inf/es/sinoa/TelaUsuario.java
@@ -1,42 +1,34 @@
 package br.ufg.inf.es.sinoa;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+import android.os.Build;
 
-public class MainActivity extends ActionBarActivity {
+public class TelaUsuario extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
+		setContentView(R.layout.activity_tela_usuario);
+
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
-				
 	}
-
-	public void vaiParaTelaUsuario(View view) {
-        Intent intent = new Intent(this, TelaUsuario.class);
-        startActivity(intent);
-    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.tela_usuario, menu);
 		return true;
 	}
 
@@ -63,8 +55,8 @@ public class MainActivity extends ActionBarActivity {
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
+			View rootView = inflater.inflate(R.layout.fragment_tela_usuario,
+					container, false);
 			return rootView;
 		}
 	}
